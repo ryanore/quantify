c4fe7dc10b99a810a0f3b05853021802bd77d19c
Ryan Ore <EMAIL>
2016-07-20 20:36:42 -0500

    Simplify app, remove resize stuff

--- src/components/app.js
+++ src/components/app.js
@@ -9,29 +9,16 @@ import AppHeader  from './app-header';
  * Top Level Application Container
  */
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {displayHeight: 600};
-    window.addEventListener('resize', this.onResize.bind(this));
-  }
-
-  componentDidMount() {
-    this.header = ReactDOM.findDOMNode(this.refs.appHeader);
-  }
-
-  onResize() {
-    const hgt = this.header.offsetHeight;
-    this.setState({displayHeight: window.innerHeight - hgt});
+  constructor(props) {
+    super(props);
   }
 
   render() {
-    console.log('render');
     return (
       <div className="app-container">
-        <AppHeader ref="appHeader"/>
+        <AppHeader />
         <DisplayGroup ref="displayGroup1" height={this.state.displayHeight} />
-        <DisplayGroup ref="displayGroup2" height={this.state.displayHeight} />
-      </div>
+       </div>
     );
   }
 }
